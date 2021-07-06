9a79a0e4030a88c2849a1eb26e14460b264ad562
Tobias de Bruijn <t.debruijn@array21.dev>
2021-07-07 00:30:57 +0200

    Initial commit: - Dependencies which are likely to be needed - Clippy lints

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![deny(deprecated)]
+#![deny(clippy::panic)]
+
+#![deny(rust_2018_idioms)]
+#![deny(clippy::decimal_literal_representation)]
+#![deny(clippy::if_not_else)]
+#![deny(clippy::large_digit_groups)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(clippy::missing_errors_doc)]
+#![deny(clippy::needless_continue)]
+
+
+fn main() {
+    println!("Hello, world!");
+}
